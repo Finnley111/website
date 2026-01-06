3d130e5f6c2cbc5a7f7a428314a7f0f3c8324c5c
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2026-01-05 22:30:04 -0500

    edit config to try to fix tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwindv4 from '@tailwindcss/vite'; // Use the v4 vite plugin
+import tailwindv4 from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://finnley111.github.io',
   base: '/',
   vite: {
-    plugins: [tailwindv4()], // Plugin goes here for v4
+    plugins: [tailwindv4()],
   },
-  integrations: [react()], // Remove tailwind() from here
+  integrations: [react()],
 });
